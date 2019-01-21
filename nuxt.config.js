0be9cc337f96caff77bf9c7af5f39960574d1c75
petar <iur.petar@gmail.com>
2019-01-21 11:25:24 +0800

    fix build error of eslint in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,20 +89,16 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    vendor: ['vue-i18n']
-    /*
-    ** You can extend webpack config here
-    */
-    // extend(config, ctx) {
-    //   // Run ESLint on save
-    //   // if (ctx.isDev && ctx.isClient) {
-    //   //   config.module.rules.push({
-    //   //     enforce: 'pre',
-    //   //     test: /\.(js|vue)$/,
-    //   //     loader: 'eslint-loader',
-    //   //     exclude: /(node_modules)/
-    //   //   })
-    //   // }
-    // }
+    vendor: ['vue-i18n'],
+    extend (config, { isDev }) {
+      if (isDev && process.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
