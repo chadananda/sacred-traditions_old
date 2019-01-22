87e4c1a11b53354887229dddc36ef1143c2ab76c
Tymofii Hrekov <thre@corp.isddesign.com>
2019-01-22 18:34:20 +0200

    [ocean] added ocean landing page

--- pages/ocean.vue
+++ pages/ocean.vue
@@ -1,19 +1,50 @@
 <template>
-  <!-- content -->
-  <Wrapper>
-    <Ocean/>
-  </Wrapper>
+  <Ocean :CDNUrl="CDNUrl" :artifacts="artifacts"/>
 </template>
 
 <script>
-import Wrapper from "~/components/wrapper/Wrapper";
 import Ocean from "~/components/ocean/Ocean.vue";
-
 export default {
-  layout: "other",
+  layout: "ocean",
+  head: function () {
+    return {
+      "title": "Ocean 2.0 Reader",
+      "meta": [
+        {"charset": "utf-8"},
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+        {"name": "keywords", "content": ""},
+        {"name": "description", "content": ""},
+        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"},
+        {"name": "msapplication-TileColor", "content": "#00aba9"},
+        {"name": "theme-color", "content": "#ffffff"}
+      ],
+      "link": [
+        {"rel": "stylesheet", "href": this.CDNUrl + "pages/styles/main.css"},
+        {"rel": "apple-touch-icon", "sizes": "180x180", "href": this.CDNUrl + "pages/favicon/apple-touch-icon.png"},
+        {"rel": "icon", "type": "image/png", "sizes": "32x32", "href": this.CDNUrl + "pages/favicon/favicon-32x32.png"},
+        {"rel": "icon", "type": "image/png", "sizes": "16x16", "href": this.CDNUrl + "pages/favicon/favicon-16x16.png"},
+        {"rel": "manifest", "href": this.CDNUrl + "pages/favicon/site.webmanifest"},
+        {"rel": "mask-icon", "href": this.CDNUrl + "pages/favicon/safari-pinned-tab.svg", "color": "#5bbad5"},
+        {"rel": "msapplication-TileColor", "content": "#00aba9"},
+        {"rel": "theme-color", "content": "#ffffff"}
+      ]
+    };
+
+  },
   components: {
-    Wrapper,
     Ocean
+  },
+  data: function () {
+    return {
+      CDNUrl: 'https://dnmxza13qkhgr.cloudfront.net/',
+      artifacts: {
+        windows:  'https://ocean-download.dev2.us/win32_setup.exe',
+        mac:      'https://ocean-download.dev2.us/mac_setup.pkg',
+        linux:    'https://ocean-download.dev2.us/linux_setup.deb',
+        android:  'https://ocean-download.dev2.us/android_setup.apk',
+        ios:      'itms-services://?action=download-manifest&url=https://ocean.bahaistudies.org/manifest-https_-ocean.bahaistudies.org-.plist'
+      }
+    }
   }
 };
 </script>
