07e173a7a7c12ed4ea74d85af1ae024c7bc8aebc
petar <iur.petar@gmail.com>
2019-01-23 11:45:40 +0800

    sort "popular" by the number of likes

--- components/articles/sidebar/WidgetPopularEntries.vue
+++ components/articles/sidebar/WidgetPopularEntries.vue
@@ -15,7 +15,19 @@
             rel="bookmark"
             class="popular-entry-title-link"
           >{{link.spancontent}}</a>
-          <span class="popular-entry-date">{{link.entrydate}}</span>
+          <div class="entry-meta">
+          <span class="posted-on">
+            <time class="entry-date" :datetime="link.entrydate">
+              {{ new Date(link.entrydate).toLocaleDateString("en-US", {year:'numeric',month:'short',day:'numeric'}) }}
+            </time>
+          </span>
+          <span class="entry-like">
+            <a href>
+              <i class="fa fa-heart-o"></i>
+              {{ link.likes }}
+            </a>
+          </span>
+             </div>
         </span>
       </li>
     </ul>
@@ -27,54 +39,63 @@
 export default {
   components: { ImageWrapper },
   data() {
+    let popular = [
+      {
+        id: 1,
+        path: "2016-09-07-lorem-ipsum-dolor-sit-amet",
+        imagepath: "article_assets/spirituality-4.jpg",
+        alt: "popular post1",
+        spanhref: "2016-09-07-lorem-ipsum-dolor-sit-amet",
+        spancontent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        entrydate: "2016-09-07",
+        likes: 42
+      },
+      {
+        id: 2,
+        path: "2016-08-14-on-enlightening-the-mind",
+        imagepath: "article_assets/What-if-Artificial-Intelligence-Was-Enlightened-1038x583.jpg",
+        alt: "popular post2",
+        spanhref: "2016-08-14-on-enlightening-the-mind",
+        spancontent: "On Enlightening the Mind",
+        entrydate: "2016-08-14",
+        likes: 5
+      },
+      {
+        id: 3,
+        path: "2016-08-02-be-like-adams-son",
+        imagepath: "article_assets/adam-and-eve-with-the-infants-cain-and-abel-lorenzo-de-ferrari.jpg",
+        alt: "popular post3",
+        spanhref: "2016-08-02-be-like-adams-son",
+        spancontent: "Be Like Adam’s Son",
+        entrydate: "2016-08-02",
+        likes: 36
+      },
+      {
+        id: 4,
+        path: "2016-03-11-essay-on-the-trinity",
+        imagepath: "article_assets/2000px-Society_of_Our_Lady_of_the_Most_Holy_Trinity_badge.svg.png",
+        alt: "popular post4",
+        spanhref: "2016-03-11-essay-on-the-trinity",
+        spancontent: "Essay on the Trinity",
+        entrydate: "2016-03-11",
+        likes: 99
+      },
+      {
+        id: 5,
+        path: "2016-03-11-developing-a-good-heart-by-dalai-lama",
+        imagepath: "article_assets/Dalailama1_20121014_4639.jpg",
+        alt: "popular post5",
+        spanhref: "2016-03-11-developing-a-good-heart-by-dalai-lama",
+        spancontent: "Developing a Good Heart",
+        entrydate: "2018-11-18",
+        likes: 87
+      }
+    ];
+    popular.sort((a, b) => {
+      return b.likes - a.likes; // sort as descending
+    });
     return {
-      links: [
-        {
-          id: 1,
-          path: "2016-09-07-lorem-ipsum-dolor-sit-amet",
-          imagepath: "article_assets/spirituality-4.jpg",
-          alt: "popular post1",
-          spanhref: "2016-09-07-lorem-ipsum-dolor-sit-amet",
-          spancontent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          entrydate: "September 7, 2016"
-        },
-        {
-          id: 2,
-          path: "2016-08-14-on-enlightening-the-mind",
-          imagepath: "article_assets/What-if-Artificial-Intelligence-Was-Enlightened-1038x583.jpg",
-          alt: "popular post2",
-          spanhref: "2016-08-14-on-enlightening-the-mind",
-          spancontent: "On Enlightening the Mind",
-          entrydate: "August 23, 2016"
-        },
-        {
-          id: 3,
-          path: "2016-08-02-be-like-adams-son",
-          imagepath: "article_assets/adam-and-eve-with-the-infants-cain-and-abel-lorenzo-de-ferrari.jpg",
-          alt: "popular post3",
-          spanhref: "2016-08-02-be-like-adams-son",
-          spancontent: "Be Like Adam’s Son",
-          entrydate: "August 2, 2016"
-        },
-        {
-          id: 4,
-          path: "2016-03-11-essay-on-the-trinity",
-          imagepath: "article_assets/2000px-Society_of_Our_Lady_of_the_Most_Holy_Trinity_badge.svg.png",
-          alt: "popular post4",
-          spanhref: "2016-03-11-essay-on-the-trinity",
-          spancontent: "Essay on the Trinity",
-          entrydate: "March 11, 2016"
-        },
-        {
-          id: 5,
-          path: "2016-03-11-developing-a-good-heart-by-dalai-lama",
-          imagepath: "article_assets/Dalailama1_20121014_4639.jpg",
-          alt: "popular post5",
-          spanhref: "2016-03-11-developing-a-good-heart-by-dalai-lama",
-          spancontent: "Developing a Good Heart",
-          entrydate: "November 18, 2018"
-        }
-      ]
+      links: popular
     };
   }
 };
