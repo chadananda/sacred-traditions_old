8fda5968c178f5e1ed92247a210d1da8778a904d
Chad <chadananda@gmail.com>
2019-01-29 20:59:25 -0500

    updated about page

--- components/about/About.vue
+++ components/about/About.vue
@@ -2,17 +2,25 @@
   <div class="site-entry">
     <article class="post format-standard hentry">
       <div class="entry-header">
-        <h1 class="entry-title">About Me</h1>
+        <h1 class="entry-title">About Us</h1>
       </div>
       <!-- .entry-header -->
       <div class="entry-content">
-        <img class="img-responsive aligncenter" src="~/assets/img/astrif-about-me.jpg">
+        <!-- <img class="img-responsive aligncenter" src="~/assets/img/astrif-about-me.jpg"> -->
         <p class="text-center">
-          Hello I’m
-          <strong>Astri Fajria</strong>. I’m 19 years old newbie Food blogger. I love to eat and make healthy food. As a good mother wanna be, my hobby is cooking. I like to share my experience about place, taste, feel that have a good food to eat, everyday. Nyamm.. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.
+          Hello. We are a group of friends of various religious traditions interested in sharing experiences
+          related to the most important part of our religion -- our book. Religion have many varied aspects but
+          the part that we share is the fact that our sacred experience is about a book -- about words.
+        </p> <p class="text-center">
+          And not just any words but words which speak to us from a higher reality. Words which change us and
+          make us something more, something better. It is the idea that God speaks and that mankind is enobled
+          through the study of God's word.
+        </p> <p class="text-center">
+          In this modern era, the world's religious traditions are no longer isolated and owned by one continent or
+          culture. Rather, the world's great religious literature represents a common heritage of the entire
+          human race. The challenge is to find in them threads of eternal truth which bind us all together in
+          a wider story.
         </p>
-        <p class="text-center">Feel free to ask me anything about you wondered.</p>
-        <h5 class="text-upper text-center">FOLLOW ME</h5>
         <IconsWrapper :icons="s_icons" iconClass="text-center"/>
         <!-- .social-icon -->
       </div>
