b6e4f85f83f8da71d67e216f20e59360e18c138f
Chad <chadananda@gmail.com>
2019-01-31 09:03:05 -0500

    added nuxt links and randomization to 'you may also like' (Posts.vue) block

--- components/article/Posts.vue
+++ components/article/Posts.vue
@@ -7,19 +7,19 @@
             <div class="col-sm-4" v-for="(article, index) in alsoLikes" :key="index">
                 <div class="post-container">
                     <div class="post-thumbnail">
-                        <a href="#">
+                        <nuxt-link :to="article.permalink">
                             <ImageWrapper :src="`${article.img}`" :alt="``" :width="223" :height="223"></ImageWrapper>
-                        </a>
+                        </nuxt-link>
                     </div>
                     <h3 class="post-title">
-                        <a href="#">
+                        <nuxt-link :to="article.permalink">
                             {{ article.title }}
-                        </a>
+                        </nuxt-link>
                     </h3>
                     <span class="post-meta">
-                        <a href="#">
+                        <nuxt-link :to="article.permalink">
                             {{ article.date }}
-                        </a>
+                        </nuxt-link>
                     </span>
                 </div>
             </div>
@@ -34,9 +34,21 @@
         name: "Posts",
         computed: {
             alsoLikes() {
-                const articles = this.$store.getters.getArticles;
-                return articles.slice(0, 3);
+                let articles = this.$store.getters.getArticles.slice() // get copy of articles
+                this.shuffleArray(articles) // randomize
+                articles = articles.slice(0, 3) // pick three
+                return articles
             }
+        },
+        methods: {
+          shuffleArray: function(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+          }
         }
     };
 </script>
