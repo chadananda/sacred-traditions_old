e3758eaae34e48e97179f41591dcc78a55dc0ef8
petar <iur.petar@gmail.com>
2019-01-31 18:22:32 +0800

    get 'also like' article from store

--- components/article/Posts.vue
+++ components/article/Posts.vue
@@ -8,7 +8,7 @@
                 <div class="post-container">
                     <div class="post-thumbnail">
                         <a href="#">
-                            <ImageWrapper :src="`${article.path}`" :alt="``" :width="223" :height="223"></ImageWrapper>
+                            <ImageWrapper :src="`${article.img}`" :alt="``" :width="223" :height="223"></ImageWrapper>
                         </a>
                     </div>
                     <h3 class="post-title">
@@ -32,25 +32,10 @@
     export default {
         components: { ImageWrapper },
         name: "Posts",
-        data: () => {
-            return {
-                alsoLikes: [
-                    {
-                        path: 'article_assets/Dhirendra-Brahmachari.jpeg',
-                        title: 'Thomas Merton Memoirs about Interfaith',
-                        date: 'Oct 27, 2016'
-                    },
-                    {
-                        path: 'article_assets/What-if-Artificial-Intelligence-Was-Enlightened-1038x583.jpg',
-                        title: 'On Enlightening the Mind',
-                        date: 'Aug 14, 2016'
-                    },
-                    {
-                        path: 'article_assets/adam-and-eve-with-the-infants-cain-and-abel-lorenzo-de-ferrari.jpg',
-                        title: 'Be Like Adam’s Son',
-                        date: 'Aug 02, 2016'
-                    }
-                ]
+        computed: {
+            alsoLikes() {
+                const articles = this.$store.getters.getArticles;
+                return articles.slice(0, 3);
             }
         }
     };
