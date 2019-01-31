1c72860d670f531deb3dcea8b89b6d811d0667ee
petar <iur.petar@gmail.com>
2019-01-31 17:06:03 +0800

    update 'You may also likes' article component

--- components/article/Posts.vue
+++ components/article/Posts.vue
@@ -3,63 +3,27 @@
         <div class="section-title">
             <h3>You May Also Like</h3>
         </div>
-        <div class="row">
-            <div class="col-sm-4">
+        <div class="row" >
+            <div class="col-sm-4" v-for="(article, index) in alsoLikes" :key="index">
                 <div class="post-container">
                     <div class="post-thumbnail">
                         <a href="#">
-                            <ImageWrapper :src="`article_assets/Dhirendra-Brahmachari.jpeg`" :alt="``" :width="223" :height="223"></ImageWrapper>
+                            <ImageWrapper :src="`${article.path}`" :alt="``" :width="223" :height="223"></ImageWrapper>
                         </a>
                     </div>
-                    <!-- .post-thumbnail -->
                     <h3 class="post-title">
-                        <a href="#">Thomas Merton Memoirs about Interfaith</a>
-                    </h3>
-                    <span class="post-meta">
-            <a href="#">Oct 27, 2016</a>
-          </span>
-                </div>
-                <!-- .post-container -->
-            </div>
-            <!-- .col-md-3 -->
-            <div class="col-sm-4">
-                <div class="post-container">
-                    <div class="post-thumbnail">
                         <a href="#">
-                            <ImageWrapper :src="`article_assets/What-if-Artificial-Intelligence-Was-Enlightened-1038x583.jpg`" :alt="``" :width="223" :height="223"></ImageWrapper>
+                            {{ article.title }}
                         </a>
-                    </div>
-                    <!-- .post-thumbnail -->
-                    <h3 class="post-title">
-                        <a href="#">On Enlightening the Mind</a>
                     </h3>
                     <span class="post-meta">
-            <a href="#">Aug 14, 2016</a>
-          </span>
-                </div>
-                <!-- .post-container -->
-            </div>
-            <!-- .col-md-3 -->
-            <div class="col-sm-4">
-                <div class="post-container">
-                    <div class="post-thumbnail">
                         <a href="#">
-                            <ImageWrapper :src="`article_assets/adam-and-eve-with-the-infants-cain-and-abel-lorenzo-de-ferrari.jpg`" :alt="``" :width="223" :height="223"></ImageWrapper>
+                            {{ article.date }}
                         </a>
-                    </div>
-                    <!-- .post-thumbnail -->
-                    <h3 class="post-title">
-                        <a href="#">Be Like Adam’s Son</a>
-                    </h3>
-                    <span class="post-meta">
-            <a href="#">Aug 02, 2016</a>
-          </span>
+                    </span>
                 </div>
-                <!-- .post-container -->
             </div>
-            <!-- .col-md-3 -->
         </div>
-        <!-- .row -->
     </div>
 </template>
 
@@ -67,7 +31,28 @@
     import ImageWrapper from '~/components/images/ImageWrapper';
     export default {
         components: { ImageWrapper },
-        name: "Posts"
+        name: "Posts",
+        data: () => {
+            return {
+                alsoLikes: [
+                    {
+                        path: 'article_assets/Dhirendra-Brahmachari.jpeg',
+                        title: 'Thomas Merton Memoirs about Interfaith',
+                        date: 'Oct 27, 2016'
+                    },
+                    {
+                        path: 'article_assets/What-if-Artificial-Intelligence-Was-Enlightened-1038x583.jpg',
+                        title: 'On Enlightening the Mind',
+                        date: 'Aug 14, 2016'
+                    },
+                    {
+                        path: 'article_assets/adam-and-eve-with-the-infants-cain-and-abel-lorenzo-de-ferrari.jpg',
+                        title: 'Be Like Adam’s Son',
+                        date: 'Aug 02, 2016'
+                    }
+                ]
+            }
+        }
     };
 </script>
 
