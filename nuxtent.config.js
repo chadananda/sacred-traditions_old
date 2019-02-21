23141bd9bbb28a76d7b589ddf90bf3cb65f30d02
Chad <chadananda@gmail.com>
2019-02-20 21:11:34 -0500

    removed unimplemented content types

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -1,63 +1,32 @@
-// const Prism = require('prismjs');
+const Prism = require('prismjs');
 
 module.exports = {
  content: [
    ["articles", {
      page: '/articles/_article',
      permalink: '/articles/:slug',
-     isPost: true,
+     isPost: false,
      generate: [ 'get', 'getAll' ]
    }],
-
-//    ["narration", {
-//     page: '/_narration',
-//     permalink: 'narration/:slug',
-//     isPost: false,
-//     generate: [ 'get', 'getAll' ]
-//    }],
-//    ["translations", {
-//     page: '/_translations',
-//     permalink: 'translations/:slug',
-//     isPost: false,
-//     generate: [ 'get', 'getAll' ]
-//    }],
-//    ["projects", {
-//      page: '/_projects',
-//      permalink: "projects/:slug",
-//      isPost: false,
-//      generate: [ 'get', 'getAll' ]
-//    }],
-//    ["courses", {
-//     page: '/_courses',
-//     permalink: '/courses/:slug',
-//     isPost: false,
-//     generate: [ 'get', 'getAll' ]
-//    }],
-//    ["events", {
-//     page: '/events/_event',
-//     permalink: 'events/:slug',
-//     isPost: false,
-//     generate: [ 'get', 'getAll' ]
-//    }],
  ],
 
     parsers: {
         md: {
             extend(config) {
-                // config.highlight = (code, lang) => {
-                //     return `<pre class="language-${lang}"><code class="language-${lang}">${Prism.highlight(code, Prism.languages[lang] || Prism.languages.markup)}</code></pre>`
-                // }
+                config.highlight = (code, lang) => {
+                    return `<pre class="language-${lang}"><code class="language-${lang}">${Prism.highlight(code, Prism.languages[lang] || Prism.languages.markup)}</code></pre>`
+                }
             }
         }
     },
 
     api: function(isStatic) {
         return {
-            // baseURL: 'http://localhost:3000',
+            baseURL: 'http://localhost:3000',
             // browserBaseURL: 'http://127.0.0.1:8080',
             // browserBaseURL: '',
-            baseURL: process.env.NODE_ENV === 'production'  ? 'https://sacred-traditions.org' : '',
-            browserBaseURL: isStatic ? 'https://sacred-traditions.org' : ''
+            // baseURL: process.env.NODE_ENV === 'production'  ? 'https://sacred-traditions.org' : '',
+            browserBaseURL: 'https://sacred-traditions.org'
         }
     }
 };
