cb9a506f29527f9c7a42919840905d5fcb993f81
petar <iur.petar@gmail.com>
2019-02-26 16:55:19 +0800

    add Slider(2- ADD VCS)

---
+++ src/components/ContentWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="content" class="site-content">
+    <div id="primary" class="content-area">
+      <main id="main" class="site-main" role="main">
+        <div class="container">
+          <slot></slot>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SkipContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="skipcontent">
+        <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        metaInfo: {
+            script: [
+                { src: '/assets/js/script.min.js', type: 'text/javascript', defer: true }
+            ]
+        }
+    }
+</script>
