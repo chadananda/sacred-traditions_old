223c548154da9f9a4d59ecefcc64f411da12b794
petar <iur.petar@gmail.com>
2019-03-10 09:09:03 +0800

    add layout in privacy component

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,95 +1,85 @@
 <template>
-  <!-- content -->
-  <!-- <ContentWrapper> -->
+  <EmptyLayout>
+    <!-- content -->
+    <!-- <ContentWrapper> -->
     <div class="row">
       <!-- article -->
       <div class="col-md-8 col-md-offset-2">
+        <h3> Privacy Policy</h3>
+        <p> <i>Your privacy is important to us. This privacy statement provides information about the personal information that FFA collects, and the ways in which we use that personal information. </i></p>
+        <h5> Personal information collection</h5>
+        <p> We may collect and use the following kinds of personal information: </p>
+        <ul>
+          <li> Information about your use of this website (including assessment tests); </li>
+          <li> Information that you provide using for the purpose of registering with the website (including email, name, phone number and age);</li>
+          <li> Information about transactions carried out over this website (including registration and enrollment);</li>
+          <li> Information that you provide for the purpose of subscribing to the website services (including email address) </li>
+        </ul>
 
+        <h5> Using personal information </h5>
+        <p> We may use your personal information to:</p>
 
-<h3> Privacy Policy</h3>
+        <ul>
+          <li>Administer this website;</li>
+          <li>Personalize the website for you;   </li>
+          <li>Enable your access to and use of the website services;   </li>
+          <li>Publish information about you on the website;   </li>
+          <li>Send to you products that you purchase;   </li>
+          <li>Supply to you services that you purchase;   </li>
+          <li>Send to you statements and invoices;   </li>
+          <li>Collect payments from you; and   </li>
+          <li>Send you marketing communications.   </li>
+        </ul>
 
-<p> <i>Your privacy is important to us. This privacy statement provides information about the personal information that FFA collects, and the ways in which we use that personal information. </i></p>
+        <p>In addition to the disclosures reasonably necessary for the purposes identified elsewhere above, we may disclose your personal information to the extent that it is required to do so by law, in connection with any legal proceedings or prospective legal proceedings, and in order to establish, exercise or defend its legal rights.</p>
 
-<h5> Personal information collection</h5>
-<p> We may collect and use the following kinds of personal information: </p>
+        <h5> Securing your data </h5>
+        <p> We will take reasonable technical and organizational precautions to prevent the loss, misuse or alteration of your personal information. <p>
+        <p> We will store all the personal information you provide on its secure servers.</p>
+        <p> Information relating to electronic transactions entered into via this website will be protected by encryption technology.</p>
 
-<ul>
-  <li> Information about your use of this website (including assessment tests); </li>
-  <li> Information that you provide using for the purpose of registering with the website (including email, name, phone number and age);</li>
-  <li> Information about transactions carried out over this website (including registration and enrollment);</li>
-  <li> Information that you provide for the purpose of subscribing to the website services (including email address) </li>
-</ul>
+        <h5> Cross-border data transfers</h5>
 
-<h5> Using personal information </h5>
-<p> We may use your personal information to:</p>
+        <p> Information that we collect may be stored and processed in and transferred between any of the countries in which we operate to enable the use of the information in accordance with this privacy policy.</p>
 
-<ul>
-  <li>Administer this website;</li>
-  <li>Personalize the website for you;   </li>
-  <li>Enable your access to and use of the website services;   </li>
-  <li>Publish information about you on the website;   </li>
-  <li>Send to you products that you purchase;   </li>
-  <li>Supply to you services that you purchase;   </li>
-  <li>Send to you statements and invoices;   </li>
-  <li>Collect payments from you; and   </li>
-  <li>Send you marketing communications.   </li>
-</ul>
+        <p> In addition, personal information that you submit for publication on the website will be published on the internet and may be available around the world.</p>
 
-<p>In addition to the disclosures reasonably necessary for the purposes identified elsewhere above, we may disclose your personal information to the extent that it is required to do so by law, in connection with any legal proceedings or prospective legal proceedings, and in order to establish, exercise or defend its legal rights.</p>
+        <p> You agree to such cross-border transfers of personal information.</p>
 
-<h5> Securing your data </h5>
-<p> We will take reasonable technical and organizational precautions to prevent the loss, misuse or alteration of your personal information. <p>
+        <h5> Updating this statement</h5>
 
-<p> We will store all the personal information you provide on its secure servers.</p>
+        <p>  We may update this privacy policy by posting a new version on this website.</p>
 
-<p> Information relating to electronic transactions entered into via this website will be protected by encryption technology.</p>
+        <p> You should check this page occasionally to ensure you are familiar with any changes.</p>
 
-<h5> Cross-border data transfers</h5>
-
-<p> Information that we collect may be stored and processed in and transferred between any of the countries in which we operate to enable the use of the information in accordance with this privacy policy.</p>
-
-<p> In addition, personal information that you submit for publication on the website will be published on the internet and may be available around the world.</p>
-
-<p> You agree to such cross-border transfers of personal information.</p>
-
-<h5> Updating this statement</h5>
-
-<p>  We may update this privacy policy by posting a new version on this website.</p>
-
-<p> You should check this page occasionally to ensure you are familiar with any changes.</p>
-
-<h5> Other websites</h5>
-<p> This website may contain links to other websites.</p>
-
-
-
-
+        <h5> Other websites</h5>
+        <p> This website may contain links to other websites.</p>
 
       </div>
       <!-- .col-md-9 -->
     </div>
     <!-- .row -->
     <!-- #content -->
-  <!-- </ContentWrapper> -->
+    <!-- </ContentWrapper> -->
+  </EmptyLayout>
 </template>
 
 <script>
-
-export default {
-  // layout: "other",
-  components: {   },
-  head() {
-    return {
-      title: 'About Sacred-Traditions.org',
-      meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-        {
-          hid: "description",
-          name: "description",
-          content: "Sacred Traditions Interfaith Project"
-        }
-      ]
+  import EmptyLayout from '~/layouts/Empty.vue'
+  export default {
+    components: { EmptyLayout },
+    head() {
+      return {
+        title: 'About Sacred-Traditions.org',
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          {
+            hid: "description",
+            name: "description",
+            content: "Sacred Traditions Interfaith Project"
+          }
+        ]
+      }
     }
   }
-}
 </script>
