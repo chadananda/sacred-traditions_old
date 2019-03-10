ba7dcc5b420c23b707b96ca3b3727b8679020e84
petar <iur.petar@gmail.com>
2019-03-11 01:48:51 +0800

    add configuration for purgeCss

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,3 +1,9 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || []
+  }
+}
+
 module.exports = {
   siteName: `sacred-traditions`,
   titleTemplate: `%s`,
@@ -10,6 +16,19 @@ module.exports = {
         path: 'content/articles/**/*.md',
         typeName: 'Article',
         route: '/articles/:slug'
+      }
+    },
+    {
+      use: 'gridsome-plugin-purgecss',
+      options: {
+        content: [
+          './src/**/*.vue',
+          './src/**/*.js',
+          './src/**/*.jsx',
+          './src/**/*.md'
+        ],
+        extractor: TailwindExtractor,
+        extensions: ['vue', 'js', 'jsx', 'md']
       }
     }
   ],
