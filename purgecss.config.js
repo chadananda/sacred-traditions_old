814530b7d53746fc5f7a497aae3ab6e75f7e4642
petar <iur.petar@gmail.com>
2019-03-11 12:19:20 +0800

    modify package.json and create purgecss.config.js

---
+++ purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './dist/**/*.html',
+    './dist/**/**/*.html',
+    './dist/assets/js/*.js',
+    './dist/assets/plugins/*.js'
+  ],
+  css: [
+    './dist/assets/css/*.css'
+  ],
+  rejected: true
+}
