8e3a6218a101d05ba929802f053e7a72a71c28ac
petar <iur.petar@gmail.com>
2019-03-13 16:39:17 +0800

    set configuration for sitemap XML

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,6 +11,39 @@ module.exports = {
         typeName: 'Article',
         route: '/articles/:slug'
       }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+        exclude: ['/exclude-me'],
+        config: {
+          '/': {
+            changefreq: 'weekly',
+            priority: 0.1
+          },
+          '/articles/*': {
+            changefreq: 'weekly',
+            priority: 0.2
+          },
+          '/about': {
+            changefreq: 'weekly',
+            priority: 0.3
+          },
+          '/contact': {
+            changefreq: 'weekly',
+            priority: 0.4
+          },
+          '/ocean': {
+            changefreq: 'weekly',
+            priority: 0.5
+          },
+          '/privacy': {
+            changefreq: 'weekly',
+            priority: 0.6
+          }
+        }
+      }
     }
   ],
   port: 3000
